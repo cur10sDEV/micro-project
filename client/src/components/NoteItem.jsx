555125bcd60367938fd0b20b22cec66f39339c24
cur10sDEV <specsx@protonmail.com>
2022-08-29 19:13:03 +0530

    added date displaying as substrings

--- client/src/components/NoteItem.jsx
+++ client/src/components/NoteItem.jsx
@@ -1,11 +1,11 @@
 const NoteItem = ({ note }) => {
 	const { title, body, updated } = note;
 	const noteHighlight = body.substring(0, 50);
-	console.log(updated);
+	const date = updated.substring(0, 10);
 	return (
 		<div className="note-item">
 			<h3 className="note-heading">{title}</h3>
-			<p className="note-date">date</p>
+			<p className="note-date">{date}</p>
 			<p className="note-body">{noteHighlight}</p>
 		</div>
 	);
